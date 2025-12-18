a1eb15110996519c347ee777e6017f2d63f20184
Pradipa Y <pdpyyo@gmail.com>
2025-12-18 18:39:54 +0700

    mengubah MainActivity.kt

--- app/src/main/java/com/example/databaseintegration/MainActivity.kt
+++ app/src/main/java/com/example/databaseintegration/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.databaseintegration.modeldata.DataSiswa
 import com.example.databaseintegration.ui.theme.DatabaseIntegrationTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             DatabaseIntegrationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DatabaseIntegrationTheme {
-        Greeting("Android")
     }
 }
