e08597f0d3b3bb746f760f7752f32ee2f471c5af
Pradipa Y <pdpyyo@gmail.com>
2025-12-18 16:21:28 +0700

    buat interface RepositoryDataSiswa di RepositoryDataSiswa.kt

--- app/src/main/java/com/example/databaseintegration/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/databaseintegration/repositori/RepositoryDataSiswa.kt
@@ -1,2 +1,12 @@
 package com.example.databaseintegration.repositori
 
+import com.example.databaseintegration.modeldata.DataSiswa
+
+interface RepositoryDataSiswa{
+    suspend fun getDataSiswa() : List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa:DataSiswa) :retrofit2.Response<Void>
+
+    //suspend fun getStatusSiswa(id:Int) : DataSiswa
+    //suspend fun editStatusSiswa(id:Int,dataSiswa: DataSiswa):retrofit2.Response<Void>
+    //suspend fun hapusStatusSiswa(id:Int):retrofit2.Response<Void>
+}
