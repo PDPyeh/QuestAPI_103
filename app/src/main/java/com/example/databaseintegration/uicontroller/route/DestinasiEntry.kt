095f5f6fb83a5844a5956c4f160b4282bb177e4f
Pradipa Y <pdpyyo@gmail.com>
2025-12-18 18:36:14 +0700

    menmambahkan file dan membuat object DestinasiEntry.kt

---
+++ app/src/main/java/com/example/databaseintegration/uicontroller/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.databaseintegration.uicontroller.route
+
+import com.example.databaseintegration.R
+
+object DestinasiEntry : DestinasiEntry {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
