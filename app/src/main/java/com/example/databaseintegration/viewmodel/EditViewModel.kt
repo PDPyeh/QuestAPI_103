75fae36af5705c12244c6491386d1e12bb3e6770
Pradipa Y <pdpyyo@gmail.com>
2025-12-25 20:49:50 +0700

    membenarkan kode di EditViewModel.kt

--- app/src/main/java/com/example/databaseintegration/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/databaseintegration/viewmodel/EditViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.databaseintegration.viewmodel
 
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
