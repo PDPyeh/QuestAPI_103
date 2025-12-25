c870dea35b179638cbd1abe9fcce30bae81a5e56
Pradipa Y <pdpyyo@gmail.com>
2025-12-25 20:55:41 +0700

    membuat class EditViewmodel di edit Viewnodel

--- app/src/main/java/com/example/databaseintegration/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/databaseintegration/viewmodel/EditViewModel.kt
@@ -17,38 +17,5 @@ import retrofit2.Response
 
 class EditViewModel( savedStateHandle: SavedStateHandle,private val repositoryDataSiswa: RepositoryDataSiswa
 ) : ViewModel() {
-    var uiStateSiswa by mutableStateOf(UIStateSiswa())
-    private set
 
-    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
-
-    init {
-        viewModelScope.launch {
-            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa).toUIStateSiswa(true)
-        }
-    }
-
-    fun updateUiState(detailSiswa: DetailSiswa) {
-        uiStateSiswa = UIStateSiswa(
-            detailSiswa = detailSiswa,
-            isEntryValid = validasiInput(detailSiswa)
-        )
-    }
-
-    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
-        return with(uiState) {
-            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
-        }
-    }
-
-    suspend fun editSatuSiswa() {
-        if (validasiInput(uiStateSiswa.detailSiswa)) {
-            val call: Response<Void> = repositoryDataSiswa.editSatuSiswa(idSiswa, uiStateSiswa.detailSiswa.toDataSiswa())
-            if (call.isSuccessful) {
-                println("Update Sukses ${call.message()}")
-            } else {
-                println("Update Error: ${call.errorBody()}")
-            }
-        }
-    }
 }
