358739349316e545c4f6e10c2b618da1cfa37dd6
Pradipa Y <pdpyyo@gmail.com>
2025-12-25 20:01:47 +0700

    menambahkan EditViewModel.kt

--- app/src/main/java/com/example/databaseintegration/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/databaseintegration/viewmodel/EditViewModel.kt
@@ -1,2 +1,53 @@
 package com.example.databaseintegration.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.databaseintegration.modeldata.DetailSiswa
+import com.example.databaseintegration.modeldata.UIStateSiswa
+import com.example.databaseintegration.modeldata.toDataSiswa
+import com.example.databaseintegration.modeldata.toUIStateSiswa
+import com.example.databaseintegration.repositori.RepositoryDataSiswa
+import com.example.databaseintegration.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class EditViewModel( savedStateHandle: SavedStateHandle,private val repositoryDataSiswa: RepositoryDataSiswa
+) : ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+    private set
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa).toUIStateSiswa(true)
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa = UIStateSiswa(
+            detailSiswa = detailSiswa,
+            isEntryValid = validasiInput(detailSiswa)
+        )
+    }
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    suspend fun editSatuSiswa() {
+        if (validasiInput(uiStateSiswa.detailSiswa)) {
+            val call: Response<Void> = repositoryDataSiswa.editSatuSiswa(idSiswa, uiStateSiswa.detailSiswa.toDataSiswa())
+            if (call.isSuccessful) {
+                println("Update Sukses ${call.message()}")
+            } else {
+                println("Update Error: ${call.errorBody()}")
+            }
+        }
+    }
+}
