0742c273af05ca622d6fcb5abbb438bf3fc8ac77
Pradipa Y <pdpyyo@gmail.com>
2025-12-25 20:26:48 +0700

    menambahkan Composable DetailSiswaScreen di HalamanDetail.kt

--- app/src/main/java/com/example/databaseintegration/view/HalamanDetail.kt
+++ app/src/main/java/com/example/databaseintegration/view/HalamanDetail.kt
@@ -43,7 +43,58 @@ import com.example.databaseintegration.viewmodel.StatusUiDetail
 import com.example.databaseintegration.viewmodel.provider.PenyediaViewModel
 import kotlinx.coroutines.launch
 
-
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailSiswaScreen(
+    navigateToEditItem: (Int) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiDetail.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        },
+        floatingActionButton = {
+            val uiState = viewModel.statusUiDetail
+            FloatingActionButton(
+                onClick = {
+                    when (uiState) {
+                        is StatusUiDetail.Success ->
+                            navigateToEditItem(uiState.statusSiswa.id)
+                        else -> {}
+                    }
+                },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = stringResource(R.string.update)
+                )
+            }
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        val coroutineScope = rememberCoroutineScope()
+        BodyDetailDataSiswa(
+            statusUiDetail = viewModel.statusUiDetail,
+            onDelete = {
+                coroutineScope.launch {
+                    viewModel.hapusSatuSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+        )
+    }
+}
 
 @Composable
 private fun BodyDetailDataSiswa(
